ab8bba639d93379f770c66d57d4f52589517ff02
ViernalisBeltre <beltreperezviernalis@gmail.com>
2025-12-05 21:48:26 -0400

    Se edito el contexto y la cadena de conexión

--- AgendaZenSpa/AgendaZenSpa/Program.cs
+++ AgendaZenSpa/AgendaZenSpa/Program.cs
@@ -1,3 +1,8 @@
+using AgendaZenSpa.Data;
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
