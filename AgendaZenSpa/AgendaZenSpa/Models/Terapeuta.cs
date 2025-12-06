776ca2b4dc53b32ec1343a1b49c2d9e09f58da67
ViernalisBeltre <beltreperezviernalis@gmail.com>
2025-12-05 23:41:30 -0400

    Modelo Terapeuta

---
+++ AgendaZenSpa/AgendaZenSpa/Models/Terapeuta.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace AgendaZenSpa.Models
+{
+    public class Terapeuta
+    {
+        [Key]
+        public int TerapeutaID { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El apellido es obligatorio.")]
+        [StringLength(100)]
+        public string Apellido { get; set; }
+
+        [StringLength(150)]
+        public string Especialidad { get; set; }
+
+        // Propiedad de navegación (Relación 1-a-Muchos con Citas)
+        public ICollection<Cita> Citas { get; set; } = new List<Cita>();
+    }
+}
