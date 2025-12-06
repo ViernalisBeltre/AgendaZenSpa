923f21c59ed7af970960d5d453396e67c98fc0eb
ViernalisBeltre <beltreperezviernalis@gmail.com>
2025-12-05 21:42:43 -0400

    Creación del contexto de la base de datos

---
+++ AgendaZenSpa/AgendaZenSpa/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AgendaZenSpa.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Paciente> Pacientes { get; set; }
+    public DbSet<Servicio> Servicios { get; set; }
+    public DbSet<Terapeuta> Terapeutas { get; set; }
+    public DbSet<Cita> Citas { get; set; }
+}
